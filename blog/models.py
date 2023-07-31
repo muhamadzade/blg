bf84da5c127f28314999fbf38d868fb58c4fd165
muhamadzade <muhamadzade@gmail.com>
2023-07-31 19:31:11 +0330

    phase6

--- blog/models.py
+++ blog/models.py
@@ -8,8 +8,8 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(default=datetime.now)
-    published_date = models.DateTimeField(default=datetime.now,blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(default=timezone.now,blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
